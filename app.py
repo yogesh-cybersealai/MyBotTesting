2acae33b4e7f7be0da19dde97bf7e1963a0a8860
Vineet Kumar <vineetkumar.vk941@gmail.com>
2024-05-28 16:47:05 +0530

    port change back to 8000

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ APP.router.add_route('*', '/api/messages', messages)
 
 if __name__ == "__main__":
     try:
-        port = 8001
+        port = 8000
         web.run_app(APP, host="0.0.0.0", port=port)
     except Exception as error:
         raise error
